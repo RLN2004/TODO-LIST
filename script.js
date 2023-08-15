fb7fdd0b2e106e91f83b329b4437edbdda6900e3
RLN <113087232+RLN2004@users.noreply.github.com>
2023-08-15 09:40:08 +0530

    VERSION - 1.0.1
    MADE THE DESGIN RESPONSIVE AND ADDED THE ADD TASK FFEATURE

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+function isEmptyOrSpaces(str){
+	if (str === null || str.match(/^\s*$/) !== null) {
+    	return false;
+    }
+    else{
+    	return true;
+    }
+}
+
 function addtask() {
 	var tsk = document.getElementById('task').value;
-	console.log(tsk)
+	var tsklist = document.getElementById('tasklist');
+
+	if (isEmptyOrSpaces(tsk)) {
+		const newtask = document.createElement("div");
+		const ntask = document.createTextNode(tsk);
+		newtask.appendChild(ntask);
+		tsklist.appendChild(newtask);
+		newtask.classList.add("task");
+	}
+	else{
+		alert("!..ENTER SOMTHING..!")
+	}
+
+	document.getElementById('task').value="";
+}
+
+function deletetask() {
+	
 }
